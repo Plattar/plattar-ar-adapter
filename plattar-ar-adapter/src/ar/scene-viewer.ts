e9e29f53582aecf044351b3f9cd834e441dfbceb
David Arayan <david.arayan12@gmail.com>
2021-08-20 16:54:52 +1000

    - Added SceneViewer implementation

---
+++ plattar-ar-adapter/src/ar/scene-viewer.ts
@@ -0,0 +1,79 @@
+import { Analytics } from "../analytics/analytics";
+
+export class SceneViewer {
+    public modelUrl: string | null = null;
+    public url: string | null = null;
+    public araction: string | null = null;
+    public titleHTML: string;
+    public isVertical: boolean = false;
+
+    constructor() {
+        this.modelUrl = null;
+        this.url = null;
+        this.titleHTML = "<b>" + document.title;
+        this.isVertical = false;
+    }
+
+    public start(): void {
+        if (!this.modelUrl) {
+            throw new Error("SceneViewer.start() - model url not set, use SceneViewer.modelUrl");
+        }
+
+        const url: string | null = this.url;
+        const araction: string | null = this.araction;
+
+        let composedLink: string | null = null;
+
+        if (url || araction) {
+            if (araction) {
+                const link: URL = new URL(location.href);
+                link.searchParams.set("araction", araction);
+                composedLink = encodeURIComponent(link.href);
+            }
+            else if (url && /^http/.test(url)) {
+                composedLink = encodeURIComponent(url);
+            }
+            else if (url && /araction=/.test(url)) {
+                const match: RegExpMatchArray | null = url.match(/araction=(.*)/gm);
+
+                if (match) {
+                    const action = match[0].split("=")[1];
+                    const link: URL = new URL(location.href);
+                    link.searchParams.set("araction", action);
+                    composedLink = encodeURIComponent(link.href);
+                }
+            }
+        }
+
+        if (!composedLink) {
+            throw new Error("SceneViewer.start() - failed to create composition link, check parameters");
+        }
+
+        const linkOverride: string = encodeURIComponent(location.href + '#no-ar-fallback');
+
+        let intent: string = 'intent://arvr.google.com/scene-viewer/1.1';
+        intent += '?file=' + this.modelUrl;
+        intent += '&mode=ar_preferred';
+        intent += '&link=' + composedLink;
+        intent += '&title=<b>' + this.titleHTML;
+
+        if (this.isVertical) {
+            intent += '&enable_vertical_placement=true';
+        }
+
+        intent += ' #Intent;scheme=https;package=com.google.ar.core;action=android.intent.action.VIEW;';
+        intent += 'S.browser_fallback_url=' + linkOverride + ';end;';
+
+        const anchor: HTMLAnchorElement = document.createElement("a");
+        anchor.setAttribute("href", intent);
+        anchor.click();
+
+        Analytics.track({
+            device: "android",
+            eventCategory: "Scene Viewer",
+            eventAction: "Start Augment"
+        });
+
+        Analytics.startRecordEngagement();
+    }
+}
