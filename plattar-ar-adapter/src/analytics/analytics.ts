7a3ce200b58c83acc7e0029e13165821b74b5a87
David Arayan <david.arayan12@gmail.com>
2021-08-23 13:37:01 +1000

    - Analytics now uses HashMap instead of arbitrary Objects to set keys and values. this is a much more robust and flexible system.

--- plattar-ar-adapter/src/analytics/analytics.ts
+++ plattar-ar-adapter/src/analytics/analytics.ts
@@ -3,25 +3,34 @@ import { Server } from "@plattar/plattar-api";
 
 export class Analytics {
 
+    private readonly _map: Map<string, any>;
+
+    constructor() {
+        this._map = new Map<string, any>();
+
+        this.push("event", "track");
+        this.push("source", "embed");
+        this.push("pageTitle", document.title);
+        this.push("pageURL", location.href);
+        this.push("referrer", document.referrer);
+        this.push("user_id", this.getUserID());
+        this.push("origin", Server.location().type);
+    }
+
     public track(dataSet: any) {
         const url: string = Server.location().analytics;
         const data = dataSet || {};
 
-        const analytic = {
-            event: "track",
-            source: "embed",
-            pageTitle: document.title,
-            pageURL: location.href,
-            referrer: document.referrer,
-            user_id: this.getUserID(),
-            origin: Server.location().type,
-            data: data
-        };
+        this.push("data", data);
 
         const xmlhttp: XMLHttpRequest = new XMLHttpRequest();
         xmlhttp.open("POST", url);
         xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        xmlhttp.send(JSON.stringify(analytic));
+        xmlhttp.send(JSON.stringify(Object.fromEntries(this._map)));
+    }
+
+    public push(key: string, value: any): void {
+        this._map.set(key, value);
     }
 
     public getUserID(): string {
