ac9fef6310901f48f984d1238e766164ec946b9b
David Arayan <david.arayan12@gmail.com>
2021-08-23 13:10:00 +1000

    - Added BasicHTTP support

---
+++ plattar-ar-adapter/src/util/basic-http.ts
@@ -0,0 +1,34 @@
+/**
+ * Simple HTTP interaction module
+ */
+export class BasicHTTP {
+    public static get(protocol: string, path: string): Promise<any> {
+        return new Promise<any>((accept, reject) => {
+            const http: XMLHttpRequest = new XMLHttpRequest();
+            http.open(protocol, path, true);
+
+            http.onload = (e) => {
+                if (http.status === 200) {
+                    if (http.response) {
+                        try {
+                            const resp: any = JSON.parse(http.response);
+                            return accept(resp);
+                        }
+                        catch (_err) { /* silent */ }
+                    }
+
+                    return accept({});
+                }
+                else {
+                    return reject(e);
+                }
+            };
+
+            http.onerror = (e) => {
+                return reject(e);
+            };
+
+            http.send(null);
+        });
+    }
+}
