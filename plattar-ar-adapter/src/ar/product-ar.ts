c54904262fbce0b21d02568adfc3932a2ad24383
David Arayan <david.arayan12@gmail.com>
2021-08-24 15:09:46 +1000

    - Added ProductAR Analytics instance and start() functionality

--- plattar-ar-adapter/src/ar/product-ar.ts
+++ plattar-ar-adapter/src/ar/product-ar.ts
@@ -1,4 +1,5 @@
 import { FileModel, Product, ProductVariation, Scene, Project } from "@plattar/plattar-api";
+import Analytics from "../analytics/analytics";
 import Util from "../util/util";
 import ARViewer from "../viewers/ar-viewer";
 
@@ -7,9 +8,15 @@ import ARViewer from "../viewers/ar-viewer";
  */
 export default class ProductAR {
 
+    // analytics instance
+    private readonly _analytics: Analytics;
+
+    // product and selected variation IDs
     private readonly _productID: string;
     private readonly _variationID: string;
 
+    // this thing controls the actual AR view
+    // this is setup via .init() function
     private _ar: ARViewer | null;
 
     constructor(productID: string | undefined | null = null, variationID: string | undefined | null = null) {
@@ -23,6 +30,7 @@ export default class ProductAR {
 
         this._productID = productID;
         this._variationID = variationID;
+        this._analytics = new Analytics();
         this._ar = null;
     }
 
@@ -34,10 +42,17 @@ export default class ProductAR {
         return this._variationID;
     }
 
+    /**
+     * Initialise the ProductAR instance. This returns a Promise that resolves
+     * successfully if initialisation is successful, otherwise it will fail.
+     * 
+     * filure can occur for a number of reasons but it generally means that AR
+     * cannot be performed.
+     */
     public init(): Promise<ProductAR> {
         return new Promise<ProductAR>((accept, reject) => {
             if (!Util.canAugment()) {
-                return reject(new Error("ProductAR.init() - cannot proceed as context is not available for AR"));
+                return reject(new Error("ProductAR.init() - cannot proceed as AR not available in context"));
             }
 
             const product: Product = new Product(this.productID);
@@ -52,9 +67,26 @@ export default class ProductAR {
         });
     }
 
-    public launch(): void {
+    /**
+     * Initialise and launch with a single function call. this is mostly for convenience.
+     * Use .init() and .start() separately for fine-grained control
+     */
+    public launch(): Promise<void> {
+        return new Promise<void>((accept, reject) => {
+            this.init().then((value: ProductAR) => {
+                value.start();
+
+                return accept();
+            }).catch(reject);
+        });
+    }
+
+    /**
+     * Launches the internal AR instance using an appropriate version of AR Viewers
+     */
+    public start(): void {
         if (!this._ar) {
-            throw new Error("ProductAR.launch() - cannot proceed as AR instance is null, did you init via ProductAR.init()?");
+            throw new Error("ProductAR.start() - cannot proceed as AR instance is null");
         }
 
         this._ar.start();
