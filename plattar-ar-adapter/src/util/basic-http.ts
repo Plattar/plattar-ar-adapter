be853d1115983153565247721d4dd2da4f5368da
David Arayan <david.arayan12@gmail.com>
2021-08-24 12:32:49 +1000

    - Made BasicHTTP a little more robust by catching all possible errors

--- plattar-ar-adapter/src/util/basic-http.ts
+++ plattar-ar-adapter/src/util/basic-http.ts
@@ -4,31 +4,37 @@
 export default class BasicHTTP {
     public static get(protocol: string, path: string): Promise<any> {
         return new Promise<any>((accept, reject) => {
-            const http: XMLHttpRequest = new XMLHttpRequest();
-            http.open(protocol, path, true);
+            try {
+                const http: XMLHttpRequest = new XMLHttpRequest();
 
-            http.onload = (e) => {
-                if (http.status === 200) {
-                    if (http.response) {
-                        try {
-                            const resp: any = JSON.parse(http.response);
-                            return accept(resp);
+                http.open(protocol, path, true);
+
+                http.onload = (e) => {
+                    if (http.status === 200) {
+                        if (http.response) {
+                            try {
+                                const resp: any = JSON.parse(http.response);
+                                return accept(resp);
+                            }
+                            catch (_err) { /* silent */ }
                         }
-                        catch (_err) { /* silent */ }
+
+                        return accept({});
                     }
+                    else {
+                        return reject(e);
+                    }
+                };
 
-                    return accept({});
-                }
-                else {
+                http.onerror = (e) => {
                     return reject(e);
-                }
-            };
+                };
 
-            http.onerror = (e) => {
+                http.send(null);
+            }
+            catch (e) {
                 return reject(e);
-            };
-
-            http.send(null);
+            }
         });
     }
 }
