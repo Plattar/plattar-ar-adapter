efadd89be7bef2f783d9992111aadb161efd796e
David Arayan <david.arayan12@gmail.com>
2022-02-08 11:31:36 +1100

    - Added prelimenary implementation of SceneAR. Requires implementing the Scene Composability functionality

--- plattar-ar-adapter/src/ar/scene-ar.ts
+++ plattar-ar-adapter/src/ar/scene-ar.ts
@@ -1,26 +1,179 @@
+import { Analytics } from "@plattar/plattar-analytics";
+import { Product, Project, Scene, SceneModel, SceneProduct, Server } from "@plattar/plattar-api";
+import { Util } from "../util/util";
+import ARViewer from "../viewers/ar-viewer";
+import QuicklookViewer from "../viewers/quicklook-viewer";
+import RealityViewer from "../viewers/reality-viewer";
+import SceneViewer from "../viewers/scene-viewer";
 import { LauncherAR } from "./launcher-ar";
 
 /**
  * Performs AR functionality related to Plattar Scenes
  */
 export class SceneAR extends LauncherAR {
+
+    // analytics instance
+    private _analytics: Analytics | null = null;
+
+    // scene and selected variation IDs
+    private readonly _sceneID: string;
+
+    // this thing controls the actual AR view
+    // this is setup via .init() function
+    private _ar: ARViewer | null;
+
+    constructor(sceneID: string | undefined | null = null) {
+        super();
+
+        if (!sceneID) {
+            throw new Error("SceneAR.constructor(sceneID) - sceneID must be defined");
+        }
+
+        this._sceneID = sceneID;
+        this._ar = null;
+    }
+
+    public get sceneID(): string {
+        return this._sceneID;
+    }
+
+    private _SetupAnalytics(scene: Scene): void {
+        let analytics: Analytics | null = null;
+
+        // setup scene stuff (if any)
+        if (scene) {
+            analytics = new Analytics(scene.attributes.application_id);
+            analytics.origin = <any>Server.location().type;
+
+            this._analytics = analytics;
+
+            analytics.data.push("type", "scene-ar");
+            analytics.data.push("sceneId", scene.id);
+            analytics.data.push("sceneTitle", scene.attributes.title);
+
+            const application: Project | undefined = scene.relationships.find(Project);
+
+            // setup application stuff (if any)
+            if (application) {
+                analytics.data.push("applicationId", application.id);
+                analytics.data.push("applicationTitle", application.attributes.title);
+            }
+        }
+    }
+
+    /**
+     * Composes a Scene into an AR Model (remote operation) that can be used to launch
+     * an AR File
+     */
+    private _ComposeScene(scene: Scene, output: "glb" | "usdz" | "vto"): Promise<string> {
+        return new Promise<string>((accept, reject) => {
+
+        });
+    }
+
+    /**
+     * Initialise the SceneAR instance. This returns a Promise that resolves
+     * successfully if initialisation is successful, otherwise it will fail.
+     * 
+     * filure can occur for a number of reasons but it generally means that AR
+     * cannot be performed.
+     */
     public init(): Promise<LauncherAR> {
-        throw new Error("Method not implemented.");
+        return new Promise<LauncherAR>((accept, reject) => {
+            if (!Util.canAugment()) {
+                return reject(new Error("SceneAR.init() - cannot proceed as AR not available in context"));
+            }
+
+            const scene: Scene = new Scene(this.sceneID);
+            scene.include(Project);
+            scene.include(SceneProduct);
+            scene.include(SceneProduct.include(Product));
+            scene.include(SceneModel);
+
+            scene.get().then((scene: Scene) => {
+                this._SetupAnalytics(scene);
+
+                const sceneOpt: any = scene.attributes.custom_json || {};
+
+                // we need to define our AR module here
+                // we are in Safari/Quicklook mode here
+                if (Util.isSafari() || Util.isChromeOnIOS()) {
+                    // we need to launch a VTO experience here
+                    // VTO requires Reality Support
+                    if (sceneOpt.anchor === "face") {
+                        if (Util.canRealityViewer()) {
+                            return this._ComposeScene(scene, "vto").then((modelUrl: string) => {
+                                this._ar = new RealityViewer();
+                                this._ar.modelUrl = modelUrl;
+
+                                return accept(this);
+                            }).catch(reject);
+                        }
+                        else {
+                            return reject(new Error("SceneAR.init() - cannot proceed as VTO AR requires Reality Viewer support"));
+                        }
+                    }
+
+                    // otherwise, load the USDZ stuff second if available
+                    if (Util.canQuicklook()) {
+                        return this._ComposeScene(scene, "usdz").then((modelUrl: string) => {
+                            this._ar = new QuicklookViewer();
+                            this._ar.modelUrl = modelUrl;
+
+                            return accept(this);
+                        }).catch(reject);
+                    }
+
+                    return reject(new Error("SceneAR.init() - cannot proceed as IOS device does not support AR Mode"));
+                }
+
+                // check android
+                if (Util.canSceneViewer()) {
+                    return this._ComposeScene(scene, "glb").then((modelUrl: string) => {
+                        this._ar = new SceneViewer();
+                        this._ar.modelUrl = modelUrl;
+
+                        return accept(this);
+                    }).catch(reject);
+                }
+
+                // otherwise, we didn't have AR available - it should never really reach this stage as this should be caught
+                // earlier in the process
+                return reject(new Error("SceneAR.init() - could not initialise AR correctly, check values"));
+            }).catch(reject);
+        });
     }
-    public launch(): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
+
     public start(): void {
-        throw new Error("Method not implemented.");
+        if (!this._ar) {
+            throw new Error("SceneAR.start() - cannot proceed as AR instance is null");
+        }
+
+        const analytics: Analytics | null = this._analytics;
+
+        if (analytics) {
+            analytics.data.push("device", this._ar.device);
+            analytics.data.push("eventCategory", this._ar.nodeType);
+            analytics.data.push("eventAction", "Start Scene Augment");
+
+            analytics.write();
+
+            analytics.startRecordEngagement();
+        }
+
+        // this was initialised via the init() function
+        this._ar.start();
     }
 
     public canQuicklook(): boolean {
-        throw new Error("Method not implemented.");
+        return this._ar && this._ar.nodeType === "Quick Look" ? true : false;
     }
+
     public canRealityViewer(): boolean {
-        throw new Error("Method not implemented.");
+        return this._ar && this._ar.nodeType === "Reality Viewer" ? true : false;
     }
+
     public canSceneViewer(): boolean {
-        throw new Error("Method not implemented.");
+        return this._ar && this._ar.nodeType === "Scene Viewer" ? true : false;
     }
 }
